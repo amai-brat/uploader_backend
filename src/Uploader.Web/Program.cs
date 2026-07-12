205b1fc11311178378db3ad52bad5dded3ff62cd
Almaz Shamilov <pepegapeepoclown4head@gmail.com>
2026-07-12 20:33:16 +0300

    [feat] add request logging

--- src/Uploader.Web/Program.cs
+++ src/Uploader.Web/Program.cs
@@ -1,3 +1,6 @@
+using Microsoft.AspNetCore.HttpLogging;
+using System.Text.Encodings.Web;
+using System.Text.Json;
 using Uploader.Feature;
 using Uploader.Feature.Extensions;
 using Uploader.Infrastructure;
@@ -8,12 +11,32 @@ builder.Configuration
     .AddJsonFile($"appsettings.{Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT")}.json", optional: true, reloadOnChange: false)
     .AddEnvironmentVariables(prefix: "ENV_");
 
+builder.Services.AddHttpLogging(logging =>
+{
+    logging.LoggingFields = HttpLoggingFields.All | HttpLoggingFields.RequestQuery;
+    logging.MediaTypeOptions.AddText("application/javascript");
+    logging.RequestBodyLogLimit = 4096;
+    logging.ResponseBodyLogLimit = 4096;
+    logging.CombineLogs = true;
+});
+
+builder.Logging.AddJsonConsole(options =>
+{
+    options.IncludeScopes = true;
+    options.UseUtcTimestamp = true;
+    options.TimestampFormat = "yyyy-MM-dd'T'HH:mm:ss";
+    options.JsonWriterOptions = new JsonWriterOptions
+    {
+        Encoder = JavaScriptEncoder.UnsafeRelaxedJsonEscaping,
+    };
+});
+
 builder.Services.AddOpenApi();
 builder.Services.AddFeature(builder.Configuration);
 builder.Services.AddInfrastructure(builder.Configuration);
 
 var app = builder.Build();
-
+app.UseHttpLogging();
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
